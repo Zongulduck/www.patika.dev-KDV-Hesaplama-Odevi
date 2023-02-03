370b6e3421b935bdf723615672e630d5a9f9eec0
Selçuk Batıhan <105587867+Zongulduck@users.noreply.github.com>
2023-02-04 01:08:43 +0300

    Update KDV.java

--- KDV.java
+++ KDV.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class KDV {
     public static void main(String[] args) {
-        double tutar,kdv,kdv2,kdvlitutar;
+        double tutar,kdv,kdv2,kdvlitutar,kdeger;
         kdv=0.18;
         kdv2=0.08;
         Scanner input = new Scanner(System.in);
@@ -9,13 +9,20 @@ public class KDV {
         tutar=input.nextDouble();
         if(tutar>1000){
             kdvlitutar=tutar+(tutar*kdv2);
+            kdeger=tutar*kdv2;
             System.out.println("kdv li tutarınız ");
             System.out.println(kdvlitutar);
+            System.out.println("kdv miktarı ; ");
+            System.out.println(kdeger);
         }
         if(tutar>0 && tutar<=1000){
             kdvlitutar=tutar+(tutar*kdv);
+            kdeger=tutar*kdv;
             System.out.println("kdv li tutarınız ");
             System.out.println(kdvlitutar);
+            System.out.println("kdv miktarı ; ");
+            System.out.println(kdeger);
+            
         }
         if(tutar<=0) {
             System.out.println("tutar>0 olmalıdır! ");
